80930d1ee2ce879e1221e97128aa8407c0cfb8d5
dengailin <729880819@qq.com>
2016-03-20 00:33:08 +0800

    add readme

---
+++ readme.md
@@ -0,0 +1,29 @@
+# wowTabber
+### A chrome extension based on es6, react and backpack.
+### What it can do
++ List all your tabs title
++ Click and go to your tab
++ Refresh after create or close a tab
++ Put the recently visited tabs in front of the list.
+
+### Preview
+![demo](./assets/demo.gif)
+
+### How to install
+    
+    // install dependence package 安装依赖
+    npm install
+
+    // build 编译输出
+    npm build
+
++ Open your chrome
++ Input "chrome://extension" at search box
++ Choose develop mode
++ Add extension and choose "build" directory
+
+### License
+MIT
+Copyright (c) 2016 Irene Deng
+
+
---
+++ webpack.production.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+var path = require('path');
+module.exports = {
+    entry: {
+        bundle: './src/js/app.js',
+        background: './src/js/background.js'
+    },
+    output: {
+        path: path.resolve(__dirname + '/build'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css!less!'
+            }
+        ]
+    }
+};
